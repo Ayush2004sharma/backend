a9a92cf1398bf7b1242a8e0a9d0a62bddb0870be
Ayush2004sharma <ayush.sharma.cseds.2022@miet.iac.in>
2025-08-09 23:23:09 +0530

    update

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ import cors from 'cors';
 import http from 'http';
 
 import appointmentRoutes from './routes/appointmentRoutes.js';
-
 import userRoutes from './routes/userRoutes.js';
 import doctorRoutes from './routes/doctorRoutes.js';
 import connectDB from './config/mongodb.js';
-
 
 const app = express();
 connectDB();
 
 const server = http.createServer(app);
 
-app.use(cors({
-  origin: (origin, cb) => cb(null, origin || '*'),
-  credentials: true
-}));
-
+// CORS config
+const allowedOrigins = [
+  process.env.FRONTEND_URL || 'http://localhost:3000'
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -28,8 +31,9 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/doctors', doctorRoutes);
 app.use('/api/appointments', appointmentRoutes);
+
 // 404 Middleware
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
@@ -43,4 +47,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
